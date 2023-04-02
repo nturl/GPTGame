8fd99f5361f0a83aeead91d550d53385177b0ec4
nturl <34818653+nturl@users.noreply.github.com>
2023-04-01 23:20:46 -0400

    Update game.js

--- game.js
+++ game.js
@@ -8,9 +8,18 @@ let playerX = (canvas.width - playerWidth) / 2;
 
 const objectWidth = 20;
 const objectHeight = 20;
-let objectX = Math.random() * (canvas.width - objectWidth);
-let objectY = 0;
 const objectSpeed = 2;
+const numberOfObjects = 5;
+let objects = [];
+
+function initObjects() {
+  for (let i = 0; i < numberOfObjects; i++) {
+    objects.push({
+      x: Math.random() * (canvas.width - objectWidth),
+      y: -objectHeight - i * (canvas.height / numberOfObjects),
+    });
+  }
+}
 
 function drawPlayer() {
   ctx.beginPath();
@@ -20,53 +29,60 @@ function drawPlayer() {
   ctx.closePath();
 }
 
-function drawObject() {
+function drawObject(x, y) {
   ctx.beginPath();
-  ctx.rect(objectX, objectY, objectWidth, objectHeight);
+  ctx.rect(x, y, objectWidth, objectHeight);
   ctx.fillStyle = "#FF0000";
   ctx.fill();
   ctx.closePath();
+}
+
+let score = 0;
+
+function drawScore() {
+  ctx.font = "16px Arial";
+  ctx.fillStyle = "#0095DD";
+  ctx.fillText("Score: " + score, 8, 20);
 }
 
 function updateGame() {
   ctx.clearRect(0, 0, canvas.width, canvas.height);
 
   drawPlayer();
-  drawObject();
+  objects.forEach((object) => drawObject(object.x, object.y));
+  drawScore();
 
-  objectY += objectSpeed;
+  objects.forEach((object) => {
+    object.y += objectSpeed;
 
-  if (objectY > canvas.height) {
-    objectX = Math.random() * (canvas.width - objectWidth);
-    objectY = 0;
-  }
-
-  if (
-    objectY + objectHeight > canvas.height - playerHeight &&
-    objectX > playerX - objectWidth &&
-    objectX < playerX + playerWidth
-  ) {
-    objectX = Math.random() * (canvas.width - objectWidth);
-    objectY = 0;
-  }
-
-  requestAnimationFrame(updateGame);
-}
-
-document.addEventListener("keydown", keyDownHandler, false);
-
-function keyDownHandler(e) {
-  if (e.key === "Right" || e.key === "ArrowRight") {
-    playerX += playerSpeed;
-    if (playerX + playerWidth > canvas.width) {
-      playerX = canvas.width - playerWidth;
+    if (object.y > canvas.height) {
+      object.x = Math.random() * (canvas.width - objectWidth);
+      object.y = -objectHeight;
     }
-  } else if (e.key === "Left" || e.key === "ArrowLeft") {
-    playerX -= playerSpeed;
-    if (playerX < 0) {
-      playerX = 0;
+
+    if (
+      object.y + objectHeight > canvas.height - playerHeight &&
+      object.x > playerX - objectWidth &&
+      object.x < playerX + playerWidth
+    ) {
+      score++;
+      object.x = Math.random() * (canvas.width - objectWidth);
+      object.y = -objectHeight;
     }
+  });
+
+  if (score >= 20) {
+    gameOver();
+  } else {
+    requestAnimationFrame(updateGame);
   }
 }
 
-updateGame();
+function gameOver() {
+  ctx.clearRect(0, 0, canvas.width, canvas.height);
+  ctx.font = "24px Arial";
+  ctx.fillStyle = "#0095DD";
+  ctx.fillText("Game Over! Final Score: " + score, canvas.width / 2 - 120, canvas.height / 2);
+}
+
+document.addEventListener("keydown", keyDownHandler,
